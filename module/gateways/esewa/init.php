97807253c09db514274e14d1b45f7d89505e164c
Yubraj Pandeya <167261861+yubrajpandeya@users.noreply.github.com>
2024-09-12 09:37:10 +0545

    Create init.php

---
+++ module/gateways/esewa/init.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * WHMCS eSewa Payment Gateway Helper Functions
+ *
+ * For more information, please refer to the online documentation.
+ *
+ * @see https://github.com/yubrajpandeya
+ *
+ * @copyright Copyright (c) Yubraj Pandeya
+ * @author : @Yubraj Pandeya
+ */
+
+require_once __DIR__ . "/helpers.php";
